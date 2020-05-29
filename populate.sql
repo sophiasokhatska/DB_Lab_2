2532c6f3a68b176d09fa52001c08d620ad32c4c4
sophiasokhatska <sophia.sokhatska@gmail.com>
2020-05-29 03:44:53 +0300

    Insert values into tables

---
+++ populate.sql
@@ -0,0 +1,74 @@
+--POKEMONS TYPES--
+INSERT INTO P_TYPES
+VALUES ('Grass', 'Special', 'Light_green');
+
+INSERT INTO P_TYPES
+VALUES ('Fire', 'Special', 'Red');
+
+INSERT INTO P_TYPES
+VALUES ('Water', 'Special', 'Blue');
+
+INSERT INTO P_TYPES
+VALUES ('Bug', 'Physical', 'Dark_green');
+
+INSERT INTO P_TYPES
+VALUES ('Normal', 'Physical', 'Grey');
+
+INSERT INTO P_TYPES
+VALUES ('Poison', 'Physical', 'Purple');
+
+INSERT INTO P_TYPES
+VALUES ('Electric', 'Special', 'Yellow');
+
+INSERT INTO P_TYPES
+VALUES ('Ground', 'Physical', 'Brown');
+
+
+--GENERATIONS--
+
+INSERT INTO GENERATIONS
+VALUES ('1', '1', '151', '1996');
+
+INSERT INTO GENERATIONS
+VALUES ('2', '152', '251', '1999');
+
+INSERT INTO GENERATIONS
+VALUES ('3', '252', '386', '2002');
+
+INSERT INTO GENERATIONS
+VALUES ('4', '387', '493', '2007');
+
+--POKEMONS--
+
+INSERT INTO POKEMONS
+VALUES ('Bulbasaur', '1', 'Grass', 'Poison', '45', '49', '49', '45', '1');
+
+INSERT INTO POKEMONS
+VALUES ('Marowak', '105', 'Ground', 'Normal', '60', '80', '110', '45', '1');
+
+INSERT INTO POKEMONS
+VALUES ('Spinarak', '167', 'Bug', 'Poison', '40', '60', '40', '30', '2');
+
+INSERT INTO POKEMONS
+VALUES ('Azumarill', '184', 'Water', 'Normal', '100', '50', '80', '50', '2');
+
+INSERT INTO POKEMONS
+VALUES ('Qwilfish', '211', 'Water', 'Poison', '65', '95', '75', '85', '2');
+
+INSERT INTO POKEMONS
+VALUES ('Surskit', '283', 'Bug', 'Water', '40', '30', '32', '65', '3');
+
+INSERT INTO POKEMONS
+VALUES ('Camerupt', '323', 'Fire', 'Ground', '70', '100', '70', '40', '3');
+
+INSERT INTO POKEMONS
+VALUES ('Bibarel', '400', 'Normal', 'Water', '79', '85', '60', '71', '4');
+
+INSERT INTO POKEMONS
+VALUES ('RotomHeat Rotom', '479', 'Electric', 'Fire', '50', '65', '107', '86', '4');
+
+INSERT INTO POKEMONS
+VALUES ('Skorupi', '451', 'Poison', 'Bug', '40', '50', '90', '65', '4');
+
+INSERT INTO POKEMONS
+VALUES ('Whiscash', '340', 'Water', 'Ground', '110', '78', '73', '60', '3');
